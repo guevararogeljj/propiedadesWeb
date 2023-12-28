290c6f1c94895fe5d266d2cc23c859ae10f777e8
jeronimoguevararogel <guevararogeljj@icloud.com>
2023-12-28 12:15:59 -0600

    update fix show

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,44 @@
 <template>
   <v-app>
-
     <div class="content">
       <router-view v-if="show" />
     </div>
-    <footerComponent v-if="show"/>
+    <footerComponent v-if="show" />
   </v-app>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue';
+<script>
 import footerComponent from './components/shared/footer.vue';
-const show = true;
-const analitics = import.meta.env.VITE_APP_G_ANALYTICS;
-onMounted(() => {
-  if (window.top === window.self) {
-    // Call the function with the provided ID
-      setupGoogleTagManager(analitics);
+export default {
+  name: 'App',
+  data() {
+    return {
+      show: true,
+      id: import.meta.env.VITE_APP_G_ANALYTICS
+    }
+  },
+  components: {
+    footerComponent
+  },
+  created() {
+    if (window.top === window.self) {
+      this.setupGoogleTagManager(this.id);
       return;
     } else {
-      show.value = false;
+      this.show = false;
       return;
     }
-});
-
-const setupGoogleTagManager = (id) => {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtm.js?id=${id}`;
-  document.head.appendChild(script);
+  },
+  methods: {
+    setupGoogleTagManager(id) {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = `https://www.googletagmanager.com/gtm.js?id=${id}`;
+      document.head.appendChild(script);
+    }
+  }
 };
-
+//
 </script>
 <style>
 .content {
@@ -47,6 +55,8 @@ const setupGoogleTagManager = (id) => {
   background-position: center;
   /* This will center the image in the card */
 }
+
 .WordSection1 {
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-}</style>
+}
+</style>
